acd10235ce0dfcfe2768d9e7744a07d135b4eef1
Sylwester Sojka <sojka.sylwester.dev@gmail.com>
2025-02-01 22:07:52 +0100

    Dodana kulaa logika wyswietlania price

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ from dotenv import dotenv_values
 import base64
 import instructor
 
+PRICE_FOR_IMAGE = 0.0000075
+PRICE_FOR_TEXT = 0.00001 
+
 from models import PaintingInfo, New_paint
 
 st.set_page_config(page_title="Painting reader", layout="centered")
 
 config = dotenv_values(".env")
 
+st.session_state["total_tokens_used"] = st.session_state.get("total_tokens_used", 0)
+
+
 def prepare_image_for_openai(image_file):
+
 
     image_data = base64.b64encode(image_file.read()).decode('utf-8')
     return f"data:image/png;base64,{image_data}"
@@ -22,6 +29,8 @@ def return_openai_instructor():
     return instructor_openai_client
 
 def generate_data_for_text(painting_details, response_model=New_paint):
+
+    print("WYKONUJE CALL OPENAI DLA TEXT")
     res = return_openai_instructor().chat.completions.create(
     model="gpt-4o",
     response_model=response_model,
@@ -42,15 +51,18 @@ def generate_data_for_text(painting_details, response_model=New_paint):
 
     response = res.model_dump()
     tokens_used = response['total_tokens_usage_cost_text_to_text']
-    st.session_state["total_tokens_used"] += tokens_used
-    print(f"PRINTUJE RESPONSE total usage: {tokens_used}")
+    tokens_price = float(tokens_used) * PRICE_FOR_TEXT
+    print(f"{tokens_price} $")
+
+    st.session_state["total_tokens_used"] += tokens_price
+    
+
 
     return response
     
-    
-
-
 def generate_data_for_image(uploaded_files, response_model=PaintingInfo):
+
+    print("WYKONUJE CALL OPENAI DLA OBRAZ")
     responses = []
     for file in uploaded_files:
         
@@ -78,23 +90,26 @@ def generate_data_for_image(uploaded_files, response_model=PaintingInfo):
         )
 
         response = res.model_dump()
-        tokens_used = response['total_tokens_usage_cost_image_to_text']          
-        st.session_state["total_tokens_used"] += tokens_used
-        
-        print(f"PRINTUJE RESPONSE total usage for image {file.name}: {tokens_used}")
+        tokens_used = response['total_tokens_usage_cost_image_to_text'] 
+        tokens_price = float(tokens_used) * PRICE_FOR_IMAGE
+        print(f"{tokens_price} $")
+
+        st.session_state["total_tokens_used"] += tokens_price
+
+
 
         responses.append(response)
-        print(responses)
+
     return responses
 
 
-st.session_state["total_tokens_used"] = st.session_state.get("total_tokens_used", 0)
+
 
 
 if not st.session_state.get("openai_key"):
     if "API_KEY" in config:
         st.session_state["openai_key"] = config["API_KEY"]
-        # st.write(st.session_state["openai_key"])
+
     
     else:
         st.info("Provide Your OPENAI API key to continue")
@@ -115,27 +130,33 @@ with st.sidebar:
                                       type=["png", "jpg", "jpeg", "gif", "bmp"])
 
     if uploaded_files:
+        
         st.success(f"Successfully loaded {len(uploaded_files)} image(s).")
 
-    st.write(st.session_state["total_tokens_used"])
+        st.write(f"Total cost: {st.session_state['total_tokens_used']}$")
+
 
 
 if uploaded_files:
 
+    st.session_state["should_generate_data"] = True
     tab_names = [file.name for file in uploaded_files]
     tabs = st.tabs(tab_names)
     
+
     with st.spinner("Generating painting details..."):
         try:
             responses = generate_data_for_image(uploaded_files=uploaded_files)
+            
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-            
+
 
     for tab, file, response in zip(tabs, uploaded_files, responses):
         with tab:
+            
             st.image(file, caption=file.name, use_container_width=True)
             st.markdown(f"**Title:** {response['title']}")
             st.markdown(f"**Author:** {response['author']}")
@@ -144,12 +165,12 @@ if uploaded_files:
             # Możemy dodać opis jako tekst w 3 zdaniach
             st.markdown(f"**Description:**")
             st.markdown(f"> {response['description_of_historical_event_in_3_sentences']}")
-            print(type(response))
+            
 
-            st.markdown(f"**Tokens used for this image:** {response['total_tokens_usage_cost_image_to_text']}")
 
             if st.button("Generate Recommendation", key=f"btn_{file.name}"):
                 reccomendation_response = generate_data_for_text(response)
+                
 
                 with st.expander("Click here for new painting recomendation :arrow_double_down:"):              
                     st.markdown(f"**Title:** {reccomendation_response['title']}")
